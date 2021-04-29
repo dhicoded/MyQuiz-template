9b56cfc537868cb62e5d8f677b43c94fd5e8bb59
dhicoded <73926505+dhicoded@users.noreply.github.com>
2021-04-29 10:28:47 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,43 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+	//write code to hide question elements
+	
+	//write code to change background color to yellow
+
+	//write code to show a heading for showing the result of Quiz
+
+	//call getContestantInfo() method of contestant class
+
+	//Follow instruction 5-8 in the project document and implement it.
+
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,28 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+
+  //If contenstant count reaches 2, change gameState to play(1)
+
+  //If gameState=1, call the play method of quiz object
+
+}
